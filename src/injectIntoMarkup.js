e20f5fca9e666f16d1c89e5ce925b4965d1aa867
Andrey Popp <8mayday@gmail.com>
2015-04-09 03:25:40 +0300

    injectIntoMarkup: readable JSON while non in prod

--- src/injectIntoMarkup.js
+++ src/injectIntoMarkup.js
@@ -2,13 +2,16 @@
  * @copyright 2015 Andrey Popp <8mayday@gmail.com>
  */
 
-import asciiJSON from 'ascii-json';
+import {escapeNonAsciis} from 'ascii-json';
 
 /**
  * Inject data and optional client scripts into markup.
  */
 export default function injectIntoMarkup(markup, data, scripts) {
-  let escapedJson = asciiJSON.stringify(data).replace(/<\//g, '<\\/');
+  let json = process.env.NODE_ENV === 'production' ?
+    JSON.stringify(data) :
+    JSON.stringify(data, null, 2);
+  let escapedJson = escapeNonAsciis(json).replace(/<\//g, '<\\/');
   let injected = `<script>window.__reactAsyncDataPacket__ = ${escapedJson}</script>`;
 
   if (scripts) {
